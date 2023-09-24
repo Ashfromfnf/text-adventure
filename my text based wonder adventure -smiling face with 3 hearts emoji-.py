d9b30c1c88a7ed943bf608504f5523f226d41bbb
Ash <43928265+Ashfromfnf@users.noreply.github.com>
2023-09-25 00:46:08 +0200

    a lot of updatess, mainly the shroom
    shroom + some changes to the duck attack sequence

--- my text based wonder adventure -smiling face with 3 hearts emoji-.py
+++ my text based wonder adventure -smiling face with 3 hearts emoji-.py
@@ -2,21 +2,23 @@ import time
 import random
 
 name = ""
+maxhp = 100
 hp = 100
 atkdmg = 10
+inventory = []
+
 firsttime = True
 swordtime = 0
-inventory = []
 
 
 def start():
     global firsttime, name
     if firsttime == True:
-        name = input("What is your name? -> ")
+        name = input("What is your name? -> ").lower()
         print("HP: " + str(hp))
         print("You enter a very dark dungeon, there are two doors, one on the left and one on the right.")
         firsttime = False
-    answer = input('Which one do you go through?: ').lower
+    answer = input('Which one do you go through?: ').lower()
 
     if answer == "left" or answer == "l":
         print("you went left!!")
@@ -42,71 +44,97 @@ def room_1():
 def room_2():
     global atkdmg, swordtime
     print("There's a sword stuck in a rock on the floor. Do you pull it out?")
-    answer = input("y/n: ").lower
+    answer = input("y/n: ").lower()
     if answer == "y" or answer == "yes":
         print("You pull out the sword! (+50 atk)")
         atkdmg += 50
         room_3()
     elif answer == "n" or answer == "no":
-        print("You don't pull out the sword and keep going")
         swordtime += 1
-        room_4()
+        room_sword()
     else:
         print("Invalid input!")
         room_2()
 
 def room_3():
-    global hp, atkdmg
-    print("You continue walking until you see a huge rubber duck! Do you attack?")
-    answer = input("y/n: ")
-    if answer == "y":
-        print("You hit the duck once!")
-        print("The duck hits you! ( " + str(100) + "-" + "40" + "=" + str(hp-40))
-        print("Do you hit the duck again?")
-        answer = input("y/n: ")
-        if answer == "y" or answer == "yes":
-            hp = 100
-            atkdmg += 60
-            print("You killed the duck! (+" + str(100-hp) + " HP, +60 ATK")
-        elif answer == "n" or answer == "no":
-            print("You died!")
+    print("You've been walking for a while, without seeing anything of interest. \n Then you stumble upon a room with nothing but a mushroom in the middle of the room. \n You ponder if you should eat the mushroom or keep going \n The mushroom might be dangerous, but it might also aid you in battle")
+    answer = input("Do you eat the mushroom? y/n: ").lower()
+    if answer == "y" or answer == "yes":
+        print("You lean down to pick the mushroom and hear it whisper in a language you do not understand.")
+        time.sleep(1)
+        print("You feel a sudden urge to eat the mushroom...")
+        time.sleep(1)
+        print("You feel like the mushroom is dangerous, but you can't look away from it...")
+        time.sleep(1)
+        print("You start to salvate while you notice yourself inching closer and closer towards the mushroom...")
+        time.sleep(1)
+        print("You take a big bite of the mushroom, you feel stronger than ever, it tastes amazing!")
+        time.sleep(0.5)
+        print("You take another bite, then another bite, and another bite, until the mushroom is eaten up. \n (+60 HP, +10 ATK)")
+        maxhp += 60
+        hp = maxhp
+        atkdmg += 10
+        time.sleep(0.75)
+        print("You stand up and feel very dizzy, you pass out.")
+        room_shroom()
+    if answer == "n" or answer == "no":
+        print("You walk past the mushroom, probably for the better. It might have been poisonous.")
+    else:
+        print("Invalid input! :/")
+        room_3()
+
+
+def room_shroom():
+    # haha, room and shroom rhymes! :D
+    print("""You wake up to 6 shroom-people dragging you along the cold dungeon floor. \n They're no taller than 50cm and they have a comically large shroomcap that almost looks like a hat \n They're chanting "Today's feast will be unmatched, we got a big, tall human, with a lot of flesh" \n They're singing and dancing, while dragging you to their camp. \n Once you arrive at their camp, their leader is sitting on his throne, with an even larger shroomcap than the others. \n He commands the other shrooms to hang you over the flame. \n You try to convince the shroomfolk that you don't taste as good as you look, they're kinda skeptical, so they carve out a decent-sized chunk of your thigh, while you're screaming and crying for help. \n """)
+
+
 def duck_attack():
-    global firsttime
-    global hp
-    global atkdmg
-    global duckhp
+    global firsttime, hp, atkdmg, maxhp, duckhp
+
+
+
     if firsttime == True:
-        duckhp = 100
+        duckhp = 200
         print("Do you attack the duck?")
-        answer = input("y/n: ")
-    if firsttime == False or answer == "y" or answer == "yes":
-        print("You hit the duck! (Duck's HP: " + str(duckhp+atkdmg), "-", str(duckhp) + ")")
-        print("The duck hits you! (HP: " + str(hp) + "-" + "40" + "=" + str(hp-40))
-        firsttime = False
+        answer = input("y/n: ").lower()
+        if answer == "y" or answer == "yes":
+            duckhp -= atkdmg
+            hp -= 40
+            print("You hit the duck! (Duck's HP: " + str(duckhp+atkdmg), "-", str(atkdmg) + ")")
+            print("The duck hits you! (HP: " + str(hp+40) + "-" + "40" + "=" + str(hp))
+            firsttime = False"
+        elif answer == "n" or answer == "no":
+            print("The duck killed you! Oh noes")
+            time.sleep(10)
     
         
-        if duckhp <= 0:
-            hp = 100
-            atkdmg += 60
-            print("You killed the duck and ate it's meat! (+" + str(100-hp) + " HP, +60 ATK")
+    if duckhp <= 0:
+        maxhp += 20
+        hp = maxhp
+        atkdmg += 60
+        print("You killed the duck and ate it's meat! (+20 max HP, You healed fully, +60 ATK")
         
-        if hp <= 0:
-            print("The duck killed you! :c")
-            time.sleep(10)
-            exit()
-
-        else:
-            print("Do you hit the duck again?")
-            answer = input("y/n: ")
-            if answer == "yes" or "y":
-                duck_attack()
-
-    elif answer == "n" or answer == "no":
-        print("The duck killed you! Oh noes")
+    if hp <= 0:
+        print("The duck killed you! :c")
         time.sleep(10)
-        exit()
+        
 
-def room_3():
+    else:
+        print("Do you hit the duck again?")
+        answer = input("y/n: ").lower()
+        if answer == "yes" or "y":
+            duckhp -= atkdmg
+            hp -= 40
+            print("You hit the duck! (Duck's HP: " + str(duckhp+atkdmg), "-", str(atkdmg) + ")")
+            print("The duck hits you! (HP: " + str(hp+40) + "-" + "40" + "=" + str(hp))
+            duck_attack()
+        elif answer == "n" or answer == "no":
+            print("The duck killed you! Oh noes")
+            time.sleep(10)
+
+
+def room_4():
     global firsttime
     global hp
     global atkdmg
@@ -114,24 +142,23 @@ def room_3():
     firsttime = True
     duck_attack()
 
-def room_4():
+def room_sword():
     global swordtime
-    if swordtime >= 0:
+    if swordtime <= 1:
         print("This is a plothole, you were supposed to pull out the sword")
         room_2()
-    if swordtime == 1:
+    elif swordtime == 2:
         print("Seriously, again?")
         room_2()
-    if swordtime == 2:
+    elif swordtime == 3:
         print("I'm tired of this...")
         print("The narrator stabs you with the sword.")
         time.sleep(10)
-        firsttime == True
-        start()
 
 
 
-print(inventory)
+print("inv:", inventory)
 start()
+firsttime = True
 
 
